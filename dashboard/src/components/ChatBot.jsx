aac6a58e199cb65007308ce4837418e76c9149b6
SagarTeotia1 <147220406+SagarTeotia1@users.noreply.github.com>
2024-04-22 18:50:13 +0530

    second commit

---
+++ dashboard/src/components/ChatBot.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ChatBot = () => {
+  return (
+    <div>
+      <h1 className="text-center my-10 text-xl">Ask any doubt from Paheli and Boojho</h1>
+      <iframe
+        title="Voiceflow ChatBot"
+        width="100%"
+        height="500px"
+        className=""
+        src="https://creator.voiceflow.com/prototype/659023df430722ce7eba20c2"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default ChatBot;
